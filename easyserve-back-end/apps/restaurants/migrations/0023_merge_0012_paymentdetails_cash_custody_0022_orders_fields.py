508d9462461f9203569c458f7fd68870a29d09cd
Nabeel Aziz <155102918+nabeelaziz742@users.noreply.github.com>
2026-08-16 15:58:13 +0500

    Fix conflicting restaurants migration branches

---
+++ easyserve-back-end/apps/restaurants/migrations/0023_merge_0012_paymentdetails_cash_custody_0022_orders_fields.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("restaurants", "0012_paymentdetails_cash_custody"),
+        ("restaurants", "0022_orders_accepted_by_waiter_orders_assigned_chef"),
+    ]
+
+    operations = []
