58be9b2d73f1164ccf3e1954f5c72373b32f4575
Nabeel Aziz <155102918+nabeelaziz742@users.noreply.github.com>
2026-08-17 02:28:30 +0500

    Add active table count to waiter dashboard

--- easyserve-back-end/apps/dashboard/repositories/waiter_repo.py
+++ easyserve-back-end/apps/dashboard/repositories/waiter_repo.py
@@ -1,6 +1,6 @@
 from django.utils.timezone import now
 from apps.restaurants.models import Orders, Table
-from apps.restaurants.constants import OrderStatus
+from apps.restaurants.constants import OrderStatus, TableState
 
 
 class WaiterRepository:
@@ -21,10 +21,17 @@ class WaiterRepository:
     @staticmethod
     def get_today_stats(user_profile):
         today_orders = WaiterRepository.get_today_orders(user_profile)
+        assigned_tables = WaiterRepository.get_assigned_tables(user_profile)
+        inactive_states = {
+            TableState.EMPTY.value,
+            TableState.CLEANING.value,
+            TableState.UNAVAILABLE.value,
+        }
 
         return {
             "total_orders": today_orders.count() or 0,
             "served_orders": today_orders.filter(order_status=OrderStatus.SERVED).count() or 0,
             "ready_orders": today_orders.filter(order_status=OrderStatus.PREPARED).count() or 0,
+            "active_tables": assigned_tables.exclude(table_state__in=inactive_states).count() or 0,
             "avg_serve_time": user_profile.avg_serve_time or "0",
         }
