a2c78ecb50c0fc92218455ed0fb98686622c8b19
Nabeel Aziz <155102918+nabeelaziz742@users.noreply.github.com>
2026-08-17 07:45:53 +0500

    Harden menu item restaurant ownership

--- easyserve-back-end/apps/owner/views/menu_items.py
+++ easyserve-back-end/apps/owner/views/menu_items.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,7 +8,7 @@ from utils.permissions import IsRestaurantOwner
 
 
 class MenuItemViewSet(ModelViewSet):
-    queryset = MenuItem.objects.all()
+    queryset = MenuItem.objects.select_related("menu__restaurant").all()
     serializer_class = MenuItemSerializer
 
     def get_permissions(self):
@@ -15,10 +16,19 @@ class MenuItemViewSet(ModelViewSet):
             return [AllowAny()]
         return [IsRestaurantOwner()]
 
-    def get_queryset(self):
-        menu_id = self.request.query_params.get("menu_id")
+    def _assert_menu_access(self, menu):
+        user = self.request.user
+        restaurant = menu.restaurant
+        allowed = (
+            restaurant.owners.filter(user_id=user.id).exists()
+            or restaurant.waiters.filter(user_id=user.id).exists()
+        )
+        if not allowed:
+            raise PermissionDenied("You do not have access to this restaurant's menu.")
 
-        queryset = MenuItem.objects.all()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        menu_id = self.request.query_params.get("menu_id")
 
         if menu_id:
             queryset = queryset.filter(menu_id=menu_id)
@@ -26,10 +36,14 @@ class MenuItemViewSet(ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
+        menu = serializer.validated_data.get("menu")
+        self._assert_menu_access(menu)
         serializer.save()
 
     def perform_update(self, serializer):
+        self._assert_menu_access(serializer.instance.menu)
         serializer.save()
 
     def perform_destroy(self, instance):
+        self._assert_menu_access(instance.menu)
         instance.delete()
