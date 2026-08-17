1e6351737815ce2f57fcf8752d9eb0a5b1d4de78
Nabeel Aziz <155102918+nabeelaziz742@users.noreply.github.com>
2026-08-17 08:23:01 +0500

    Register restaurant lifecycle signals

--- easyserve-back-end/apps/restaurants/apps.py
+++ easyserve-back-end/apps/restaurants/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RestaurantsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.restaurants'
+
+    def ready(self):
+        from . import signals  # noqa: F401
